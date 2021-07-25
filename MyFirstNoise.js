96832d4b8768f51316e26314adb3706d00d392d1
Erik Johannes Kostic <ejk@Eriks-Air.fritz.box>
2021-07-25 20:57:37 +0200

    initial commit

---
+++ MyFirstNoise.js
@@ -0,0 +1,24 @@
+function setup() {
+    let c = createCanvas(3000, 3000);
+//    background(0, 161, 219);
+    background(33, 65, 33);
+
+    for (var j = 0; j < height; j++){
+        var points = [];
+        for (var i = 0; i < width; i++){
+            randx = random(30);
+            randy = random(40);
+            append(points, createVector(i*10+randx, 80*j/2+randy));
+        }
+        for (var i = 0; i < width-1; i++){
+            strokeWeight(0.5);
+            //stroke(random(0),random(0),random(255));  //for blue
+            stroke(random(0),random(0),random(0));  // for green
+            line(points[i].x, points[i].y, points[i+1].x, points[i+1].y);
+        }
+    }
+    saveCanvas(c, 'myNoise', 'png');
+}
+
+function draw() {
+}
